3e541368384064ce2f65f91b99472b0f8254ce3f
Dennis Vuong <vuong.dennis@gmail.com>
2022-11-18 14:05:33 -0800

    feat: mongodb connection

---
+++ server/routes/record.js
@@ -0,0 +1,55 @@
+const express = require("express");
+
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const recordRoutes = express.Router();
+
+// This will help us connect to the database
+const dbo = require("../db/conn");
+
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
+
+// This section will help you get a list of all the records.
+recordRoutes.route("/record").get(function (req, res) {
+    let db_connect = dbo.getDb("posts");
+    db_connect
+        .collection("records")
+        .find({})
+        .toArray(function (err, result) {
+            if (err) throw err;
+            res.json(result);
+        });
+});
+
+// This section will help you get a single record by id
+recordRoutes.route("/record/:id").get(function (req, res) {
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    db_connect
+        .collection("records")
+        .findOne(myquery, function (err, result) {
+            if (err) throw err;
+            res.json(result);
+        });
+});
+
+// This section will help you create a new record.
+recordRoutes.route("/record/add").post(function (req, response) {
+    let db_connect = dbo.getDb();
+    let myobj = {
+        text: req.body.text,
+        likes: req.body.likes,
+        time: req.body.time,
+        company: req.body.company,
+        type: req.body.type,
+    };
+    db_connect.collection("records").insertOne(myobj, function (err, res) {
+        if (err) throw err;
+        response.json(res);
+    });
+});
+
+module.exports = recordRoutes;
